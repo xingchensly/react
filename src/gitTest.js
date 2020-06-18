0020994a9d4ed93b1c103a9564f341347fdddd16
leonlysu <leonlysu@tencent.com>
2020-06-18 14:29:56 +0800

    test1

--- src/gitTest.js
+++ src/gitTest.js
@@ -1,8 +1,1 @@
-console.log('test gitsssd222ss1')
-console.log('test gitsss2s111111')
-
-console.log('test gitsss2s111111')
-console.log('test gitsss2s111111')
-console.log('test gitsss2s111111')
-console.log('test gitsss2s')
-console.log("updassssdfsdfsdfsdfte")
+console.log('test 1')
