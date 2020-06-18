4f9de70cb224c7dc8d3fa8bdc75b8db92f9162d7
xingchensly <30857850+xingchensly@users.noreply.github.com>
2020-06-18 14:26:54 +0800

    Update gitTest.js

--- src/gitTest.js
+++ src/gitTest.js
@@ -2,5 +2,6 @@ console.log('test gitsssd222ss1')
 console.log('test gitsss2s111111')
 
 console.log('test gitsss2s111111')
+console.log('test gitsss2s111111')
 console.log('test gitsss2s')
 console.log("updassssdfsdfsdfsdfte")
